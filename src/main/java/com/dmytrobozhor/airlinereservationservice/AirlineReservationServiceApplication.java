4cc0eef61c791c3329cc462168917b3a019a64d2
DmytroBozhor <dimaastop1@gmail.com>
2023-12-06 16:56:35 +0100

    project init

---
+++ src/main/java/com/dmytrobozhor/airlinereservationservice/AirlineReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dmytrobozhor.airlinereservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineReservationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dmytrobozhor/airlinereservationservice/AirlineReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmytrobozhor.airlinereservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
